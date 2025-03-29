7988344f747a14ef8b3aa1698b39a01f6a9e5b9a
rauxw <hardikraut909@gmail.com>
2025-03-29 14:30:59 +0530

    Added Star

--- react_multicss/src/App.jsx
+++ react_multicss/src/App.jsx
@@ -1,13 +1,15 @@
 import Accordian from "./components/accordian";
 import Indogo from "./components/accordian/index1";
 import XRandomColor from "./components/randomColor/XRandomColor";
+import Star from "./components/star-rating/Star";
 
 function App() {
   return (
     <>
       {/* <Accordian /> */}
       {/* <Indogo /> */}
-      <XRandomColor />
+      {/* <XRandomColor /> */}
+      <Star />
     </>
   );
 }
---
+++ react_multicss/src/components/star-rating/Star.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./Star.css";
+
+const Star = ({ noOfStar = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStar)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            size={40}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Star;
